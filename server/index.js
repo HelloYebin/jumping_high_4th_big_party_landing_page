dc33859baa8d151388440f816763c0718fc0bcfe
Yebin <yebin2821@gmail.com>
2022-11-01 19:34:11 +0900

    delete, insert get data

--- server/index.js
+++ server/index.js
@@ -18,10 +18,28 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//공지사항 db 삭제하기
+app.delete("/api/:id", function (req, res) {
+  const id = req.params.id;
+  const sqlQuery = "DELETE FROM notice WHERE id = ?";
+  db.query(sqlQuery, [id], (err, result) => {
+    res.send("삭제가 완료되었습니다.");
+  });
+});
+
+//공지사항 db 가져오기
+app.get("/api/get", (req, res) => {
+  const sqlQuery = "SELECT * FROM notice ORDER BY id DESC";
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  });
+});
+
+// db에 글 저장
 app.post("/api/insert", (req, res) => {
   const title = req.body.title;
   const content = req.body.content;
-  const sqlQuery = "INSERT INTO simpleboard (title, content) VALUES (?,?)";
+  const sqlQuery = "INSERT INTO notice (title, description) VALUES (?,?)";
   db.query(sqlQuery, [title, content], (err, result) => {
     res.send("success!");
   });
